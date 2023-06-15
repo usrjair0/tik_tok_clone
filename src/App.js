1044ac2b3000a38d53f106080725220d0f7fcb2a
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-06-14 22:31:48 -0300

    commit some changes1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Video from "./pages/Video"
 
 function App() {
   return (
     <div className="App">
+      <div className='app__videos'>
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
---
+++ src/pages/Video.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./video.css"
+
+function Video() {
+  return (
+    <div className=''>
+        Eu sou um componente video
+    </div>
+  )
+}
+
+export default Video
