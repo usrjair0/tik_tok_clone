8e1655a808a6ddf059735e79694a1577fcd0823d
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-06-18 18:20:30 -0300

    finalizando mais algumas questões

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import './App.css';
-import Video from "./pages/video"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Video from "./pages/video";
+import db from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  const [video, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCollection = collection(db, "videos");
+    const videosSnapshot = await getDocs(videosCollection);
+    const videosList = videosSnapshot.docs.map((doc) => doc.data());
+    setVideos(videosList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
     <div className="App">
-      <div className='app__videos'>
-        <Video 
-          likes={300}
-          messages={200}
-          shares={300}
-          name="Paulo"
-          description="Brecker o goleiro"
-          music="música épica"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
-        />
-        
-        <Video 
+      <div className="app__videos">
+        {video.map((item) => {
+          return (
+            <Video
+              likes={item.likes}
+              messages={item.messages}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              music={item.music}
+              url={item.url}
+            />
+          );
+        })}
+
+        {/* <Video 
           likes={444}
           messages={555}
           shares={600}
@@ -23,8 +43,7 @@ function App() {
           description="gato olhando para dedo"
           music="clap your hands"
           url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
-        />
-
+        /> */}
       </div>
     </div>
   );
