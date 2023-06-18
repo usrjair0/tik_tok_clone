5e6450296437dd53c1fef18b8d4281f6cd988f43
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-06-18 17:54:18 -0300

    config some things firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore/lite'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCu0Kev6YGf04s63h8tjVFniOx6ZQzxE7Q",
   authDomain: "tik-tok---jornada-c75db.firebaseapp.com",
@@ -15,3 +12,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
